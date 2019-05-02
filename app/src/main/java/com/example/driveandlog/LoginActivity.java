25c74ca362dd99e044f243b5e78539e22cbd7b74
Unknown <mathh17@student.sdu.dk>
2019-05-02 12:15:33 +0200

    maps starter på éns egen pos

--- app/src/main/java/com/example/driveandlog/LoginActivity.java
+++ app/src/main/java/com/example/driveandlog/LoginActivity.java
@@ -102,7 +102,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         nextButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                startActivity(new Intent(LoginActivity.this, Profile.class));
+                startActivity(new Intent(LoginActivity.this, Map_page.class));
             }
         });
     }
