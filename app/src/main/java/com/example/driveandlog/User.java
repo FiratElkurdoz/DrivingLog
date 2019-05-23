e50966997d0ca111ed7e15566ee2e093f75ad940
DuneZerna <duzer17@student.sdu.dk>
2019-05-23 08:57:45 +0200

    Merge branch 'master' of C:\Users\dunez\Documents\GitKraken\DrivingLog with conflicts.

--- app/src/main/java/com/example/driveandlog/User.java
+++ app/src/main/java/com/example/driveandlog/User.java
@@ -1,32 +1,7 @@
 package com.example.driveandlog;
 
-import androidx.annotation.NonNull;
-import androidx.room.ColumnInfo;
-import androidx.room.Entity;
-import androidx.room.PrimaryKey;
-
-@Entity(tableName = "user_table")
 public class User {
 
 
 
-    @PrimaryKey(autoGenerate = true)
-    @NonNull
-    @ColumnInfo(name = "userID")
-    private int mUserID;
-    @NonNull
-    private String mUsername;
-
-    public  User(@NonNull int userID, @NonNull String mUsername){
-        this.mUserID = userID;
-        this.mUsername = mUsername;
-    }
-
-    public int getUserID(){
-        return this.mUserID;
-    }
-
-    public String getUsername(){
-        return this.mUsername;
-    }
 }
