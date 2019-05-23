19f386ef886c78167ca5da32c36432c1dafdd51d
FiratElkurdoz <svite@live.dk>
2019-05-23 14:21:10 +0200

    Log book

--- app/src/main/java/com/example/driveandlog/LoginActivity.java
+++ app/src/main/java/com/example/driveandlog/LoginActivity.java
@@ -144,7 +144,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         nextButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                startActivity(new Intent(LoginActivity.this, Map_page.class));
+                startActivity(new Intent(LoginActivity.this, com.example.driveandlog.List.class));
             }
         });
     }
