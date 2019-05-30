7f22854cbdeb29606c7bc18ea80fa59a0295c56b
DuneZerna <duzer17@student.sdu.dk>
2019-05-30 19:04:49 +0200

    Lifecycle added

--- app/src/main/java/com/example/driveandlog/LogIn.java
+++ app/src/main/java/com/example/driveandlog/LogIn.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class LogIn extends AppCompatActivity {
 
@@ -22,5 +23,47 @@ public class LogIn extends AppCompatActivity {
 
         SignUp.setOnClickListener(v -> startActivity(new Intent(LogIn.this, SignUp.class)));
 
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        Toast.makeText(getApplicationContext(), "Lifecycle: Started", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+
+        Toast.makeText(getApplicationContext(), "Lifecycle: Paused", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        Toast.makeText(getApplicationContext(), "Lifecycle: Resumed", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+
+        Toast.makeText(getApplicationContext(), "Lifecycle: Stopped", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+
+        Toast.makeText(getApplicationContext(), "Lifecycle: Destroyed", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+
+        Toast.makeText(getApplicationContext(), "Lifecycle: Restarted", Toast.LENGTH_LONG).show();
     }
 }
